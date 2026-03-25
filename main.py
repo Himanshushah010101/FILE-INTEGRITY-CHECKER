c659fb6b976f3032798a663c7970d7eeab8276f2
Himanshu shah <himanshushah010101@gmail.com>
2026-03-25 11:27:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+print("FILE INTEGRITY CHECKER WITH CLI TOOL")
+
+# THIS PROGRAM WILL FIND A TEXT FILE HASH VALUES AND CHANGED OR TEMPERED BY ANYONE SO YOU CAN FIND EASILY HELP BY HASH VALUES!!!  
+
+import hashlib
+
+def calculate_hash(file_path):
+    hash_algo = hashlib.sha256()
+
+    try:
+        with open(file_path, 'rb') as f:
+            while chunk := f.read(4096):
+                hash_algo.update(chunk)
+        return hash_algo.hexdigest()
+    except FileNotFoundError:
+        return None
+
+file = input("Enter file path: ")
+hash_value = calculate_hash(file)
+
+if hash_value:
+    print("File Hash:", hash_value)
+else:
+    print("File not found!")
